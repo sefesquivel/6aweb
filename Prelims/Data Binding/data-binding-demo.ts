0ea847afcf022caab8455f59e82d65003c4d84e7
sefesquivel <jesquivel@gmail.com>
2025-11-19 09:49:15 +0800

    Add files via upload

---
+++ Prelims/Data Binding/data-binding-demo.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-data-binding-demo',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './data-binding-demo.component.html',
+  styleUrl: './data-binding-demo.component.css'
+})
+export class DataBindingDemoComponent {
+    title = "My First App!"
+    description = "This is my new Angular Application"
+
+    imageUrl = '../assets/logo-angular.png';
+    w = 50;
+    h = 50;
+    altText = 'Angular Logo';
+
+    textColor='blue';
+
+    isHighlighted = true;
+
+    yourName = '';
+
+
+    count = 0;
+    increment() {
+      this.count++;
+    }
+    decrement() {
+      this.count--;
+    }
+    
+
+  }
+
